32a2a1a5f0bfd29641eff4605de2be81a5f38c7c
Khaled Tarek <kingdodo987@gmail.com>
2021-09-26 02:25:15 +0200

    solved 1 question in 5 mins ! proud

--- python_grinding.py
+++ python_grinding.py
@@ -138,3 +138,118 @@
 #     mini = min(numbers)
 #     second_min = numbers[1]
 #     return mini + second_min
+
+# def sum_two_smallest_numbers(numbers):
+#     first_min = min(numbers)
+#     numbers.remove(first_min)
+#     second_min = min(numbers)
+#     return first_min + second_min
+
+#  learning lambda functions
+# def double(n):
+#     return n * 2
+
+
+# double = lambda n: n * 2
+# larger = lambda a, b: a if a > b else b
+# return a if a > b else return b
+# names = ['alan', 'gregory', 'zlatan', 'jonas', 'tom', 'augustine']
+# names.sort(key=lambda x: len(x))
+# print(names)
+
+# using for loop and append()
+# numbers = [1, 2, 3, 4, 5]
+# squared_nums = []
+# square = lambda n: n ** 2
+# for num in numbers:
+#     squared_nums.append(square(num))
+# print(squared_nums)
+
+# using map() and lambda(functions)
+
+# numbers = [1, 2, 3, 4, 5]
+# squared_nums = list(map(lambda n: n ** 2, numbers))
+# print(squared_nums)
+
+# def longest(a1, a2):
+#     return ''.join(sorted(dict.fromkeys(a1 + a2)))
+#      return ''.join(sorted(set(a1 + a2)))
+# print(longest("loopingisfunbutdangerous", "lessdangerousthancoding"))
+
+# my solution
+# def validate_pin(pin):
+#     if pin.isdecimal():
+#         if len(pin) == 4:
+#             return True
+#         elif len(pin) == 6:
+#             return True
+#         return False
+#     return False
+# another solution
+# def validate_pin(pin):
+#     return len(pin) in (4, 6) and pin.isdigit()
+
+
+# print(validate_pin("1"))
+# print(validate_pin("12"))
+# print(validate_pin("12345"))
+# print(validate_pin("1234567"))
+# print(validate_pin("-1234"))
+# print(validate_pin("-12345"))
+# print(validate_pin("1.234"))
+# print(validate_pin("00000000"))
+# mine
+# def printer_error(s):
+#     alphabet_list = 'abcdefghijklm'
+#     letters = []
+#     for letter in s:
+#         if letter not in alphabet_list:
+#             letters.append(letter)
+#     return f'{len(letters)}/{len(s)}'
+
+# not mine
+# def printer_error(s):
+#     return "{}/{}".format(len([x for x in s if x not in "abcdefghijklm"]), len(s))
+
+# mine but failed cus i put the return elsewhere
+# def printer_error(s):
+#     good_colors = "abcdefghijklm"
+#     counter = 0
+#     for i in s:
+#         if i not in good_colors:
+#             counter += 1
+#     return str(counter) + "/" + str(len(s))
+# print(printer_error("aaaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbmmmmmmmmmmmmmmmmmmmxyz"))
+# print(printer_error("kkkwwwaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbmmmmmmmmmmmmmmmmmmmxyz"))
+# print(printer_error("kkkwwwaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbmmmmmmmmmmmmmmmmmmmxyzuuuuu"))
+
+# import  math
+# def find_next_square(sq):
+#     root = math.sqrt(sq)
+#     if int(root + 0.5) ** 2 == sq:
+#         root += 1
+#         return pow(root,2)
+#     return -1
+
+
+# def open_or_senior(data):
+#
+#     for info in data:
+#         print(data)
+#         age, handicap = info
+#         if age >= 55 and handicap > 7:
+#             return 'Senior'
+#         return 'Open'
+def open_or_senior(data):
+    for inner_list in data:
+        print(inner_list)
+        for info in inner_list:
+            age, handicap = info
+            #         print(data)
+            if age >= 55 and handicap > 7:
+                return 'Senior'
+            return 'Open'
+
+
+print(open_or_senior([(45, 12), (55, 21), (19, -2), (104, 20)]))
+print(open_or_senior([(16, 23), (73, 1), (56, 20), (1, -1)]))
